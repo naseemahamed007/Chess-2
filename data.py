7f7818f847ce9c235a64ce25bc7d3d2a0be6ae26
naseemahamed007 <naseemishere0@gmail.com>
2026-03-05 03:15:28 +0530

    Create data.py

---
+++ data.py
@@ -0,0 +1,19 @@
+# data.py
+
+tournaments = {
+    "main": {
+        "name": "Chennai Open Chess Championship",
+        "date": "28 March 2026",
+        "fee": "₹150",
+        "mode": "Online",
+        "platform": "Chess.com",
+        "time": "10 Minutes Rapid",
+
+        "about": "Official tournament by Nas Matrix Chess.",
+
+        "contact": {
+            "phone": "7094602011",
+            "email": "naseemishere0@gmail.com"
+        }
+    }
+}
